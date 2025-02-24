9b6228b642ce15a22f29a8b4dd67211f7e3ec216
eyeAeye <setters_dibs_8o@icloud.com>
2025-02-24 19:40:53 +0900

    deploy script updated to push gh-pages branch automatically.

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,20 @@ cd ..
 
 echo "⚡ Building Hugo site..."
 hugo
-
 echo "✅ Build complete!"
+
+echo "📌 Committing and pushing changes in gh-pages (public folder)..."
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+msg="rebuilding site $(date)"
+if [ $# -eq 1 ]; then
+  msg="$1"
+fi
+git commit -m "$msg"
+git push origin gh-pages
+cd ..
+
+# add commit for main branch later if needed
